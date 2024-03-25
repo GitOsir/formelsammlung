900a312e6ed4beec96f108d2200ad8d24bd660db
Krausi <johankrause999@gmail.com>
2024-03-25 19:34:49 +0100

    grids erstellt und formate angepasst

--- app/ui/subjects/mathematics/johan.tsx
+++ app/ui/subjects/mathematics/johan.tsx
@@ -1,11 +1,21 @@
 
 
 export default function Johan(){
+    const formula = "$$\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$"
     return(
-        <main>
+        <>
     
         <h1>mathedingens</h1>
-        
-        </main>
+        <p>{formula}</p>
+        <section>
+            <div className="grid lg:grid-cols-3 gap-12 m-12 sm:grid-cols-1">
+            <div className="border text-center h-32 rounded-md shadow-md">Dinonuggies</div>
+            <div className="border text-center h-32 rounded-md">Chickennuggies</div>
+            <div className="border text-center h-32 rounded-md">fridge</div>
+
+
+            </div>
+        </section>
+        </>
     )
 }
