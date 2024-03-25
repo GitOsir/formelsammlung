999d721970796f6c935fb4fc49effc289d213e92
DoM PC <dominikmieth@gmx.de>
2024-03-25 19:34:58 +0100

    Tabelle hinzugefügt und formate angepasst

--- app/ui/subjects/mathematics/dominik.tsx
+++ app/ui/subjects/mathematics/dominik.tsx
@@ -4,6 +4,7 @@
 export default function Dominik(){
 
 
+    const formular = "$$\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$"
 
     return(
         <>
@@ -12,6 +13,16 @@ export default function Dominik(){
             <h1 className="text-center font-bold hover:font-thin">Das ist der erste Test von mir</h1>
             <h2>Überschrift nummer 2</h2>
             <p>$$c^2 = a^2 + b^2$$</p>
+            <p>{formular}</p>
+            <section>
+                <div className="grid lg:grid-cols-3 gap-12 mx-10 sm:grid-cols-1">
+                    <div className="border text-center h-32 rounded-md shadow-md">Dingens</div>
+                    <div className="border text-center h-32 rounded-md">dingens 2</div>
+                    <div className="border text-center h-32 rounded-md">dingens 3</div>
+
+
+                </div>
+            </section>
             <div className="h-96"></div>
             <div className="h-96"></div>
             <div className="h-96"></div>
