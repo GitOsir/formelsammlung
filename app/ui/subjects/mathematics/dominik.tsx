61a63c43aa91ad2fdf9e4c6265ff9ccec6388a50
David Klinge PC <davidnormanklinge@gmail.com>
2024-03-25 19:38:32 +0100

    merged johan

--- app/ui/subjects/mathematics/dominik.tsx
+++ app/ui/subjects/mathematics/dominik.tsx
@@ -3,7 +3,7 @@
 
 export default function Dominik(){
 
-
+    const content = "$$\\sqrt{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$";
 
     return(
         <>
@@ -12,6 +12,7 @@ export default function Dominik(){
             <h1 className="text-center font-bold hover:font-thin">Das ist der erste Test von mir</h1>
             <h2>Überschrift nummer 2</h2>
             <p>$$c^2 = a^2 + b^2$$</p>
+            <p>{content}</p>
             <div className="h-96"></div>
             <div className="h-96"></div>
             <div className="h-96"></div>
