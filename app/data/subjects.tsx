7cdd86b08eef12fa77431067b1731efaf8778032
David Klinge PC <davidnormanklinge@gmail.com>
2024-03-25 23:17:54 +0100

    improved navbar and added colors to subjects

--- app/data/subjects.tsx
+++ app/data/subjects.tsx
@@ -1,10 +1,10 @@
 
-interface Subject{name:string,href:string,icon:string}
+interface Subject{name:string,href:string,color?:string,icon:string}
 
-const mathematik:Subject = {name:"Mathematik",href:"/subjects/mathematics",icon:""}
-const physik:Subject = {name:"Physik",href:"/subjects/physics",icon:""}
-const chemie:Subject = {name:"Chemie",href:"/subjects/chemistry",icon:""}
-const informatik:Subject = {name:"Informatik",href:"/subjects/compSC",icon:""}
+const mathematik:Subject = {name:"Mathematik",href:"/subjects/mathematics",color:"bg-sky-500/90",icon:""}
+const physik:Subject = {name:"Physik",href:"/subjects/physics",color:"bg-green-500/70",icon:""}
+const chemie:Subject = {name:"Chemie",href:"/subjects/chemistry",color:"bg-yellow-400/70",icon:""}
+const informatik:Subject = {name:"Informatik",href:"/subjects/compSC",color:"bg-violet-400",icon:""}
 
 
 export const subjects = [mathematik,physik,chemie,informatik]
--- app/ui/subjects/Navbar.tsx
+++ app/ui/subjects/Navbar.tsx
@@ -1,12 +1,10 @@
-"use client"
+
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Navlinks from "./Navlinks";
-import clsx from 'clsx';
 
 export default function Navbar(){
-    const pathname = usePathname();
 
 
 
@@ -14,14 +12,17 @@ export default function Navbar(){
     return (
         <>
           <div className="flex items-center w-full h-20 p-2 sticky top-0">
-            <div className="w-full h-full bg-gray-500/30 rounded backdrop-filter backdrop-blur-md shadow-lg">
+            <div className="w-full h-full bg-slate-900/70 rounded backdrop-filter backdrop-blur-md shadow-lg">
 
                 <div className="mx-5 grid grid-cols-2 h-full">
                     <div className="flex items-center">
                     <Navlinks/>
                     </div>
                     <div className="flex items-center justify-end">
-                    <p className="text-white text-2xl font-bold">FORMELSAMMLUNG</p>
+                      <div className="flex items-center text-white text-2xl font-bold bg-gray-500 px-3 py-1 rounded-s-md rounded-e-3xl">
+                      <p className="hidden lg:block">FORMELSAMMLUNG</p>
+                      <div className="bg-white h-6 w-6 ml-3 rounded-2xl"></div>
+                      </div>
                     </div>
                 </div>
 
