b2fbc6395306c3e24b0ef4299a95debfda0a2c03
DoM PC <dominikmieth@gmx.de>
2024-03-26 14:09:26 +0100

    Vektor als Test eingefügt

--- app/ui/subjects/mathematics/dominik.tsx
+++ app/ui/subjects/mathematics/dominik.tsx
@@ -1,3 +1,4 @@
+import { Archivo_Narrow } from "next/font/google"
 
 
 
@@ -5,20 +6,22 @@ export default function Dominik(){
 
 
     const formular = "$$\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$"
+    const testvektor = "$$ y = \\begin{bmatrix} 1 \\cr 1 \\cr 1 \\end{bmatrix}$$"
 
     return(
         <>
 
-            <p>Dominik ... npm run dev</p>
-            <h1 className="text-center font-bold hover:font-thin">Das ist der erste Test von mir</h1>
-            <h2>Überschrift nummer 2</h2>
-            <p>$$c^2 = a^2 + b^2$$</p>
+            <p className="text-center font-bold">Vektoren</p>
+            <h1 className="underline font-bold">1. Einführung</h1>
+            <br></br>
             <p>{formular}</p>
+            <p>$$c^2 = a^2 + b^2$$</p>
+            <p>{testvektor}</p>
             <section>
                 <div className="grid lg:grid-cols-3 gap-12 mx-10 sm:grid-cols-1">
-                    <div className="border text-center h-32 rounded-md shadow-md">Dingens</div>
-                    <div className="border text-center h-32 rounded-md">dingens 2</div>
-                    <div className="border text-center h-32 rounded-md">dingens 3</div>
+                    <div className="border text-center h-64 rounded-md shadow-lg">Betrag (Norm) eines Vektors</div>
+                    <div className="border text-center h-64 rounded-md shadow-lg">Skarlarprodukt</div>
+                    <div className="border text-center h-64 rounded-md shadow-lg">Vektorkreuzprodukt</div>
 
 
                 </div>
@@ -26,6 +29,7 @@ export default function Dominik(){
             <div className="h-96"></div>
             <div className="h-96"></div>
             <div className="h-96"></div>
+
         </>
     )
 }
