9c50e570b3ecba57383d8ed4518b87fcd62ec152
DoM PC <dominikmieth@gmx.de>
2024-03-27 12:08:20 +0100

    const hinzugefügt

--- app/ui/subjects/mathematics/dominik.tsx
+++ app/ui/subjects/mathematics/dominik.tsx
@@ -5,8 +5,8 @@ import { Archivo_Narrow } from "next/font/google"
 export default function Dominik(){
 
 
-    const formular = "$$\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$"
-    const testvektor = "$$ y = \\begin{bmatrix} 1 \\cr 1 \\cr 1 \\end{bmatrix}$$"
+    const vector_variable = "$$ y = \\begin{bmatrix} x \\cr y \\cr z \\end{bmatrix} \\longrightarrow$$"
+    const vector_numbers = "$$\\begin{bmatrix} 1 \\cr 1 \\cr 1 \\end{bmatrix}$$"
 
     return(
         <>
@@ -14,20 +14,17 @@ export default function Dominik(){
             <p className="text-center font-bold">Vektoren</p>
             <h1 className="underline font-bold">1. Einführung</h1>
             <br></br>
-            <p>{formular}</p>
             <p>$$c^2 = a^2 + b^2$$</p>
-            <p>{testvektor}</p>
-            <section>
+            <section>  
+            <div className="border text-center h-64 rounded-md shadow-lg my-10 mx-10 p-5">
+                        <p>{vector_variable} {vector_numbers}</p>
+            </div>
                 <div className="grid lg:grid-cols-3 gap-12 mx-10 sm:grid-cols-1">
-                    <div className="border text-center h-64 rounded-md shadow-lg">Betrag (Norm) eines Vektors</div>
+                    <div className="border text-center h-64 rounded-md shadow-lg m">Betrag (Norm) eines Vektors</div>
                     <div className="border text-center h-64 rounded-md shadow-lg">Skarlarprodukt</div>
                     <div className="border text-center h-64 rounded-md shadow-lg">Vektorkreuzprodukt</div>
-
-
                 </div>
             </section>
-            <div className="h-96"></div>
-            <div className="h-96"></div>
             <div className="h-96"></div>
 
         </>
