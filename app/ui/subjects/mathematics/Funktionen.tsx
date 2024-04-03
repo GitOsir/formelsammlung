372ab34ce1c2f8910133ffa216bad5a2e29d30ff
Krausi <johankrause999@gmail.com>
2024-04-03 23:49:43 +0200

    Formeln hinzugefuegt und neue karten benutzt

--- app/ui/subjects/mathematics/Funktionen.tsx
+++ app/ui/subjects/mathematics/Funktionen.tsx
@@ -1,7 +1,10 @@
+import Card from "../Card"
 
 
 export default function Funktionen(){
     const formula = "$$\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$"
+    const pythagoras = "$$ a^2 + b^2 = c^2 $$"
+    const halb = "$$ c = \\sqrt[2]{a^2 + b^2} $$"
     return(
         <>
     
@@ -9,11 +12,40 @@ export default function Funktionen(){
         <p>{formula}</p>
         <section>
             <div className="grid lg:grid-cols-3 gap-12 m-12 sm:grid-cols-1">
-            <div className="border text-center h-32 rounded-md shadow-md">Dinonuggies</div>
-            <div className="border text-center h-32 rounded-md">Chickennuggies</div>
-            <div className="border text-center h-32 rounded-md">fridge</div>
-            <p>erster commit von laptop johan</p>
+                <Card contents={
 
+                <>
+                    <h1 className="font-bold text-center">formeln und so</h1>
+                    <p>Was ist eine Formel?</p>
+                </>
+                
+                }/>
+                <Card contents={
+
+                <>
+                    <h1 className="font-bold text-center">andere Formeln</h1>
+                    <p>Das ist eine Formel</p>
+                    <div className="flex justify-center">
+                        
+                        <p >{ pythagoras }</p>
+                        
+                    </div>
+                </>
+
+                }/>
+                <Card contents={
+
+                <>
+                    <h1 className="font-bold text-center">andere Formeln</h1>
+                    <p>das ist keine formel</p>
+                    <div className="flex justify-center">
+                        
+                        <p >{ halb }</p>
+                        
+                    </div>
+                </>
+
+                }/>
             </div>
         </section>
         </>
